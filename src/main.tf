fc5a3ee2709e9882c111dad18ed253ce672baa4d
Davi Vilarinho <davi.vilarinho@exacode.com.br>
2022-05-27 18:11:57 -0300

    starting infrastructure created

---
+++ src/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.66.0"
+    }
+  }
+
+  backend {
+    bucket = "vilarinho-state"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" 
+
+  default_tags {
+    tags = {
+      Application = "base"
+      Group = "terraform-managed"
+    }
+  }
+}
